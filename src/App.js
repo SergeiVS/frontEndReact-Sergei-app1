b0f69e6c4b7cb9bfe45d487fed02dcbf0734369a
SergeiVS <fam.smirnoff@gmail.com>
2024-06-26 22:18:04 +0200

    Lesson02

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 
+// Lessons
+import Lesson02 from "./lessons/Lesson02/Lesson02";
+
+//Homeworks
+
 function App() {
   return (
     <div className="App">
-      <p>This is our first react app</p>
+      <Lesson02 />
     </div>
   );
 }
---
+++ src/lessons/Lesson02/Lesson02.js
@@ -0,0 +1,13 @@
+import Button from "../../components/button/Button";
+import "./styles.css";
+function Lesson02() {
+  return (
+    <div className="lesson02-component">
+      <p>Lesson02</p>
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default Lesson02;
